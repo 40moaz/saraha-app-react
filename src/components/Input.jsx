5301e56be06a02b92de50eb21821f6bcb29e54f7
40moaz <amoaz14109@gmail.com>
2026-02-01 21:01:54 +0200

    ed

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -26,7 +26,7 @@ const Input = () => {
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ sender: "Anonymous", text }),
+          body: JSON.stringify({ text }),
         }
       );
 
